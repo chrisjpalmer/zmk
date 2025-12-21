89bef3229ca3c2248fb42fcabb83be736baad741
chrisjpalmer <chrisjpalmer@acme.com>
2025-12-21 14:29:55 +1100

    switch to sticky keys for control layer

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd $HOME/projects/zmk/app
 
-west build -d build/left -p -b nice_nano_v2 -- -DSHIELD=cradio_left \
+west build -d build/left -b nice_nano_v2 -- -DSHIELD=cradio_left \
   -DZMK_CONFIG="$HOME/projects/zmk-ferris/config"
 
-west build -d build/right -p -b nice_nano_v2 -- -DSHIELD=cradio_right \
+west build -d build/right -b nice_nano_v2 -- -DSHIELD=cradio_right \
   -DZMK_CONFIG="$HOME/projects/zmk-ferris/config"
 
 cp $HOME/projects/zmk/app/build/left/zephyr/zmk.uf2 $HOME/projects/zmk-ferris/left.uf2
