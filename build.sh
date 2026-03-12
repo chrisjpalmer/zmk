0048c9ed5aefe9dfb28914a1f1ed953d437eda79
chrisjpalmer <chrisjpalmer@acme.com>
2026-03-12 21:05:01 +1100

    update build script

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+conda activate zmk
+
 cd $HOME/projects/zmk/app
 
-west build -d build/left -b nice_nano -- -DSHIELD=cradio_left \
-  -DZMK_CONFIG="$HOME/projects/zmk-ferris/config"
+west build -d build/left -b "nice_nano//zmk" -- \
+  -DZMK_CONFIG="$HOME/projects/zmk-ferris/config" \
+  -DSHIELD=cradio_left
 
-# west build -d build/right -b nice_nano -- -DSHIELD=cradio_right \
-#   -DZMK_CONFIG="$HOME/projects/zmk-ferris/config"
+west build -d build/right -b "nice_nano//zmk" -- \
+  -DZMK_CONFIG="$HOME/projects/zmk-ferris/config" \
+  -DSHIELD="cradio_right"
 
 cp $HOME/projects/zmk/app/build/left/zephyr/zmk.uf2 $HOME/projects/zmk-ferris/left.uf2
-# cp $HOME/projects/zmk/app/build/right/zephyr/zmk.uf2 $HOME/projects/zmk-ferris/right.uf2
+cp $HOME/projects/zmk/app/build/right/zephyr/zmk.uf2 $HOME/projects/zmk-ferris/right.uf2
